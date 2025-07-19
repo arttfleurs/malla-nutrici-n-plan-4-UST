6dd176c83012b5c81c31a14dc2f1217d047459db
arttfleurs <piamonse9@gmail.com>
2025-07-19 16:10:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function mostrarContenido(ramo) {
+  let mensaje = "";
+
+  switch (ramo) {
+    case 'anatomia':
+      mensaje = "Anatomía: estructura del cuerpo humano, sistema óseo, muscular, etc.";
+      break;
+    case 'quimica':
+      mensaje = "Química General: compuestos, enlaces, reacciones básicas.";
+      break;
+    case 'bioquimica':
+      mensaje = "Bioquímica: proteínas, enzimas, metabolismo.";
+      break;
+    case 'fisiologia':
+      mensaje = "Fisiología: funciones de órganos y sistemas.";
+      break;
+    default:
+      mensaje = "Ramo no registrado aún.";
+  }
+
+  alert(mensaje);
+}
 
